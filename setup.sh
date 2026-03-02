994a751a11c74900cf1225d0c5f71c9a2cb69798
eivarin <rodrigommpereira309@gmail.com>
2026-03-02 12:34:31 +0000

    feat: major cli usage improvements

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 chmod +x core-gui
-docker-compose pull
+COMPOSE="docker compose"
+if which docker-compose &> /dev/null; then
+    COMPOSE="docker-compose"
+fi
+$COMPOSE build core
 echo Run "./core-gui" to start the program
